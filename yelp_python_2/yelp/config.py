34229888943cb3a6fb90e33a0d9804f535917a20
Huyen Nguyen <nguyenkhanhhuyen4523@gmail.com>
2019-11-28 11:08:22 -0500

    add file

---
+++ yelp_python_2/yelp/config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+API_ROOT_URL = "https://api.yelp.com"
+
+BUSINESS_PATH = "/v3/businesses/{business_id}"
+REVIEW_PATH = "/v3/businesses/{id}/reviews"
+PHONE_SEARCH_PATH = "/v3/businesses/search/phone"
+SEARCH_PATH = "/v3/businesses/search"
+
